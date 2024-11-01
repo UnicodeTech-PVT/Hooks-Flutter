d841d7f645005339beea188e572643ed8e5eef0e
UnicodeTech <unicodetech786@gmail.com>
2024-11-01 19:36:15 +0500

    Initial Commit

---
+++ packages/flutter_hooks/lib/src/hooks.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart'
+    show
+        Brightness,
+        DraggableScrollableController,
+        ExpansionTileController,
+        WidgetStatesController,
+        SearchController,
+        TabController,
+        WidgetState;
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+
+import 'framework.dart';
+
+part 'animation.dart';
+part 'async.dart';
+part 'draggable_scrollable_controller.dart';
+part 'expansion_tile_controller.dart';
+part 'fixed_extent_scroll_controller.dart';
+part 'focus_node.dart';
+part 'focus_scope_node.dart';
+part 'keep_alive.dart';
+part 'listenable.dart';
+part 'listenable_selector.dart';
+part 'misc.dart';
+part 'page_controller.dart';
+part 'platform_brightness.dart';
+part 'primitives.dart';
+part 'scroll_controller.dart';
+part 'search_controller.dart';
+part 'tab_controller.dart';
+part 'text_controller.dart';
+part 'transformation_controller.dart';
+part 'widgets_binding_observer.dart';
+part 'widget_states_controller.dart';
+part 'debounced.dart';
